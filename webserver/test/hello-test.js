691164f952621e7813f1d9fbdc4bfeeb5bc2e8d7
nadamdb <nagyadam.db@gmail.com>
2019-03-11 23:10:50 +0100

    try nodejs travis ci #4

--- webserver/test/hello-test.js
+++ webserver/test/hello-test.js
@@ -4,8 +4,8 @@ var assert = require('chai').assert;
 var hello = require('../src/hello');
 
 describe('hello', function() {
-	  it("Returns a string of hello plus the name that was passed.", function() {
-		      var output = hello.sayHello("Dave");
-		      assert.equal(output, "Hello Dave.");
-		    });
+  it("Returns a string of hello plus the name that was passed.", function() {
+      var output = hello.sayHello("Dave");
+      assert.equal(output, "Hello Dave.");
+    });
 });
