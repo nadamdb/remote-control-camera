76deeeae61b4da9e471a6d38a0930a403e32df5d
Peter Takacs <peterta12@gmail.com>
2019-03-19 10:07:34 +0100

    stats page + datatables + viewbox jquery plugins

---
+++ website/js/stats.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$('#stat-table').DataTable();
+
+	$(function(){
+		$('.image-link').viewbox({
+			setTitle: true,
+			margin: 20,
+			resizeDuration: 300,
+			openDuration: 200,
+			closeDuration: 200,
+			closeButton: true,
+			navButtons: true,
+			closeOnSideClick: true,
+			nextOnContentClick: true
+		});
+	});
+});
