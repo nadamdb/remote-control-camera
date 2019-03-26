184488f6905ed7c50c1a87fc082542fcf5e45986
Csontho Marcell <csonthomarci@gmail.com>
2019-03-26 09:42:51 +0100

    JSON support

--- imageserver/server.js
+++ imageserver/server.js
@@ -1,65 +1,92 @@
 "use strict";
+// Imports
 const http = require("http");
 const timestamp = require('time-stamp');
-const fileUpload = require('express-fileupload');
-
 const express = require("express");
+const fileUpload = require('express-fileupload');
+var sqlite3 = require('sqlite3').verbose();
+
 const app = express();
 const httpServer = http.createServer(app);
 const PORT = process.env.PORT || 3000;
-
-var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./movements.db');
-var path_req = require('path');
 
-var URL_this_running_on = "";
-httpServer.listen(3000, () => {
-  console.log(`Server is listening on port ${PORT}`);
+// Important! Where to move the uploaded file on the server. 
+let path = '/root/agilis/public/uploads/';
+  
+
+// Answer is JSON or HTLM site. (HTML used for testing)
+var legacyHTMLformat = false;
+// Used for ?? (I don't really know)
+var URL_this_running_on = ""; //"http://csontho.info:3000";
+
+// Start HTTP server. Listening on PORT 3000, or paramter (PORT=33 node server.js starts server on 33 port)
+httpServer.listen(PORT, () => {
+  console.log('[Image Server]['+timestamp.utc('YYYY-MM-DD_HH-mm-ss')+'] '+`Server is listening on port ${PORT}`);
 });
 
 
-// default options
+// Use express-fileupload to upload images
 app.use(fileUpload());
 
-// Server /public dir and index.html site for easy upload
+// Serving static files, like uploaded images
 app.use(express.static('public'));
+
+// /upload site to post images (ex.: curl -X POST http://csontho.info:3000/upload -F sampleFile=@image.png )
 app.post('/upload', function(req, res) {
   if (Object.keys(req.files).length == 0) {
-    
-    var status_400_response = "<html><head><title>Upload failed!</title></head><body> Upload failed."+' <a href="'+URL_this_running_on+'/index.html">Take me back to the upload form</a> / <a href="'+URL_this_running_on+'/movements"> Visit /movements site</a>'
-    return res.status(400).send(status_400_response);
+    if(legacyHTMLformat) {
+      var status_400_response = "<html><head><title>Upload failed!</title></head><body> Upload failed."+' <a href="'+URL_this_running_on+'index.html">Take me back to the upload form</a> / <a href="'+URL_this_running_on+'/movements"> Visit /movements site</a>'
+      return res.status(400).send(status_400_response);
+    } else {
+      return res.status(400).send(400);
+    }
   }
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let sampleFile = req.files.sampleFile;
   let filename = sampleFile.name;
+  // Check if file is jpg or png
   let sampleFile_exttension = filename.slice(filename.length-3, filename.length);
   if(sampleFile_exttension === "jpg" || sampleFile_exttension === "png"){
-    console.log("File was ok: "+filename);
+    console.log("[Image Server]["+timestamp.utc('YYYY-MM-DD_HH-mm-ss')+"] File format was ok: "+filename);
     }
   else {
-    return res.status(400).send('Wrong format.');
+    if(legacyHTMLformat){
+      return res.status(400).send('Wrong format.');
+    }else {
+      return res.status(400).send(400);
+    }
+    
   }
-  // Use the mv() method to place the file somewhere on your server
+  
+  // Timestamp format
   var timeStamp = timestamp.utc('YYYY-MM-DD_HH-mm-ss');
-  let path = '/root/agilis/public/uploads/';
-  let imageName = 'movement'+timeStamp+'.png'
+  // Rename image to static format. (Like movement2019-03-26_10-23-33.png);
+  let imageName = 'movement'+timeStamp+'.'+sampleFile_exttension;
   let url = path+imageName;
+  // Use the mv() method to place the file somewhere on your server. NEeds path variable!
   sampleFile.mv(url, function(err) {
     if (err)
       return res.status(500).send(err);
-    var success_response = "<html><head><title>Upload complete!</title></head><body> Upload completed. File: "+imageName + ' uploaded to the server. <a href="'+URL_this_running_on+'/index.html">Take me back to the upload form</a> / <a href="'+URL_this_running_on+'/movements"> Visit /movements site</a>'
-    res.send(success_response);
-    console.log(imageName);
+    var success_response = "<html><head><title>Upload complete!</title></head><body> Upload completed. File: "+imageName + ' uploaded to the server. <a href="'+URL_this_running_on+'index.html">Take me back to the upload form</a> / <a href="'+URL_this_running_on+'/movements"> Visit /movements site</a>'
+    if(legacyHTMLformat){
+      res.send(success_response);
+    }else {
+      res.send(200);
+    }
+    
+    console.log('[Image Server]['+timeStamp+'] Image "'+sampleFile.name+'" uploaded to the server. Renamed to "'+imageName+'"');
     db.run('INSERT INTO movements(date_time, path, image) VALUES(?, ?, ?)', [timeStamp, url, imageName], (err) => {
       if(err) {
-        return console.log(err.message); 
+        return console.log('[Image Server]['+timestamp.utc('YYYY-MM-DD_HH-mm-ss')+'] '+err.message); 
       }
-      console.log('Row was added to the table.');
+      console.log('[Image Server]['+timeStamp+'] Row was added to the table.');
     })
   });
 });
 
+if(legacyHTMLformat){
 app.get("/movements", function(req, res){
     //res.send('Hello world!');
     var html_begin='<html><head><title>Detected movements</title><link rel="stylesheet" type="text/css" href="styles/styles.css"></head><body><div class="body-div"><table><tr><th>Date & Time of movement</th><th>Image name</th><th>Path in server</th><th>Small size image</th><th></th></tr>';
@@ -68,7 +95,7 @@ app.get("/movements", function(req, res){
 
     db.all(sql, [], (err, rows) => {
       if (err) {
-        console.log(err);
+        console.log('[Image Server]['+timestamp.utc('YYYY-MM-DD_HH-mm-ss')+'] '+err);
       res.send('Table is missing.');
       return;
       }
@@ -82,3 +109,29 @@ app.get("/movements", function(req, res){
     res.send(response);
     });
 });
+}
+else {
+  app.get("/movements", function(req, res){
+    var sql = "SELECT * FROM movements";
+
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        console.log('[Image Server]['+timestamp.utc('YYYY-MM-DD_HH-mm-ss')+'] '+err);
+      res.send('Table is missing.');
+      return;
+      }
+      
+      res.writeHead(200, {"Content-Type": "application/json"});
+      var answer = "";
+      var json_front='{ "movements": ['
+      var json_end = ']}'
+      rows.forEach((row) => {
+        var tmpImageUrl = 'uploads/'+row.image;
+        answer = '{"timestamp":"'+row.date_time+'", "url":"'+URL_this_running_on+'/'+tmpImageUrl+'","path":"'+row.path+'"},'+answer;
+        });
+    answer = answer.substring(0, answer.length - 1);
+    answer = json_front+answer+json_end;
+    res.end(answer);
+    });
+});
+}
