1eec2f994c6e0ed057f566c276a19fd3afe5bd7f
Paradid <paradidavid@hotmail.com>
2019-05-09 14:12:54 +0200

    Finalizing IP addresses
    Finalizing IP addresses in the picture sender and movement notify scripts.

--- raspberry/scripts/motion_detected.py
+++ raspberry/scripts/motion_detected.py
@@ -1,6 +1,6 @@
 import requests
 
 
-r= requests.put('http://80.211.171.119:3000/motion')
+r= requests.put('http://192.168.1.111:3000/motion')
 
 print(r.status_code)
--- raspberry/scripts/picture_saved.py
+++ raspberry/scripts/picture_saved.py
@@ -6,7 +6,7 @@ list_of_files = glob.glob('/home/pi/MotionImages/*')
 latest_file = max(list_of_files, key=os.path.getctime)
 
 with open(latest_file, 'rb') as f:
-	r = requests.post('http://80.211.171.119:3000/upload',files={'sampleFile':f})
+	r = requests.post('http://192.168.1.111:3000/upload',files={'sampleFile':f})
 print(r.status_code)
 
 if(r.status_code == requests.codes.ok):
